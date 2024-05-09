3ee0184997a4e6efe9922d0743c2aa9483a02b44
lee <lsljw@daum.net>
2024-05-09 13:39:05 +0900

    data insert

--- src/components/Main/Post.js
+++ src/components/Main/Post.js
@@ -5,6 +5,9 @@ function Post(props){
         <div id='Post'>
             <span>{props.id}</span>
             <span>{props.name}</span>
+            <span>{props.kor}</span>
+            <span>{props.eng}</span>
+            <span>{props.mat}</span>
         </div>
     )
 }
--- src/components/Main/PostList.js
+++ src/components/Main/PostList.js
@@ -4,7 +4,11 @@ import Post from './Post';
 function PostList(props){
 
     const result=props.postList.map(
-        (data)=>(<Post id={data.id} name={data.name}/>)
+        (data)=>(<Post id={data.id} 
+            name={data.name}
+            kor={data.kor}
+            eng={data.eng}
+            mat={data.mat}/>)
     )
 
     return(
